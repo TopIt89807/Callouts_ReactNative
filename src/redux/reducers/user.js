598633ca2461b5e9d8dd6637b5c2e1d1e63952e9
admin <topit89807@gmail.com>
2018-01-16 19:54:20 +0200

    install redux, saga and other necessary packages.

---
+++ src/redux/reducers/user.js
@@ -0,0 +1,12 @@
+import Immutable from 'seamless-immutable';
+import { createReducer } from 'reduxsauce';
+
+export const initialState = Immutable({
+
+})
+
+const handler = {
+
+}
+
+export default createReducer(initialState, handler);
---
+++ src/redux/setup.js
@@ -0,0 +1,25 @@
+import { fromJS } from 'immutable';
+import createSagaMiddleware from 'redux-saga';
+import { applyMiddleware, createStore, compose } from 'redux';
+
+import reducer from './reducers';
+import sagas from './sagas';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const setupStore = (initialState = fromJS({})) => {
+    const middlewares = [
+        sagaMiddleware,
+    ];
+    const enhancers = [
+        applyMiddleware(...middlewares),
+    ];
+
+    const store = createStore(reducer, initialState, compose(...enhancers));
+
+    sagas.forEach(saga => sagaMiddleware.run(saga));
+
+    return setupStore;
+}
+
+export default setupStore;
